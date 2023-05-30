722d2a74dbf048de47835af65cb72cb9a7fff81f
dyllamt <maxwell.dylla@gmail.com>
2023-05-29 17:00:46 -0700

    expectation for plotly express test

--- tests/chains/test_plotly_express.py
+++ tests/chains/test_plotly_express.py
@@ -5,5 +5,5 @@ from langchain_eda.chains.plotly_express import plotly_express_chain
 
 def test_plotly_express(dataframe):
     result = plotly_express_chain(dataframe)("How does movie score vary over time?")
-    assert result["plot"].__name__ == px.line.__name__
+    assert result["plot"].__name__ == px.scatter.__name__
     assert result["kwargs"] == '{"x": "year", "y": "score", "color": null}'
